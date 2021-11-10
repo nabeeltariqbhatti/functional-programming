473e536d332a8cb6f127430ee927ee7762ad33ea
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-11-10 12:02:16 +0500

    bi predicate and bipredicate with other predicates have been tested

--- src/main/java/predicate/_Predicate.java
+++ src/main/java/predicate/_Predicate.java
@@ -1,5 +1,6 @@
 package predicate;
 
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class _Predicate {
@@ -8,7 +9,14 @@ public class _Predicate {
 
 
         System.out.println(isPhoneNumberValid("03074567223"));
-        System.out.println(isPhoneNumberValidFunctional.test("03452345243"));
+        System.out.println("if phone number is valid and contains 3 "+ isPhoneNumberValidFunctional.or(doesPhoneNumberContains).test("03074567223"));
+
+        System.out.println("if phone number is valid or contains 3 "+ isPhoneNumberValidFunctional.or(doesPhoneNumberContains).test("030745672"));
+
+
+        if(biPredicate.test(isPhoneNumberValidFunctional, doesPhoneNumberContains)){
+            System.out.println("bi predicate with string is tested");
+        }
     }
 
 
@@ -18,4 +26,11 @@ public class _Predicate {
      return phoneNumber.startsWith("03") && phoneNumber.length() == 11;
 
     }
+    static Predicate<String> doesPhoneNumberContains = phoneNumber -> phoneNumber.contains("3");
+
+
+    static BiPredicate<Predicate,Predicate> biPredicate = (length, containMentChecker) -> length.test("03456754323") && containMentChecker.test("03456754323");
+
+
+
 }
