504c33924a620174fc73c572f53856494c75deb4
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-11-18 16:03:19 +0500

    builder

---
+++ src/main/java/effectivejava/Stack.java
@@ -0,0 +1,54 @@
+package effectivejava;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+ class Stack {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+    public Object pop() {
+        if (size == 0)
+            throw new EmptyStackException();
+
+        Object result = elements[--size];
+        elements[size] = null;
+
+        return result;
+    }
+    /**
+     * Ensure space for at least one more element, roughly
+     * doubling the capacity each time the array needs to grow.
+     */
+    private void ensureCapacity() {
+        if (elements.length == size)
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+    }
+}
+class StackTestDrive{
+
+
+    public static void main(String[] args) {
+
+        Stack stack = new Stack();
+
+        int i =0;
+        while(true){
+
+            stack.push( Math.random());
+
+            if(i ==1000) break;
+            i++;
+        }
+
+
+    }
+
+}
