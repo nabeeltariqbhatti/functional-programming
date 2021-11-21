67cbf2f34dbaea82419fc921af810dcae377b7ef
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-11-21 21:35:53 +0500

    ....

--- src/main/java/annotation/MainExample.java
+++ src/main/java/annotation/MainExample.java
@@ -1,6 +1,8 @@
 package annotation;
 
+import java.awt.color.ICC_Profile;
 import java.lang.annotation.Annotation;
+import java.util.*;
 
 public class MainExample {
 
@@ -17,6 +19,33 @@ public class MainExample {
         System.out.println(annotation.name());
         System.out.println(annotation.city());
 
+
+        Map<Integer, Integer> _map = new HashMap<>();
+
+
+
+        _map.put(12,34);
+
+        _map.put(13,23);
+
+        _map.put(13,12);
+
+
+//        _map.entrySet().stream().forEach(System.out::println);
+
+        Iterator<Map.Entry<Integer,Integer>> it = _map.entrySet().iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+
+//
+//  for(Map.Entry<Integer,Integer> entry : _map.entrySet()){
+//      System.out.println(entry.getKey()+  "-> " + entry.getValue() );
+//  }
+//
+//for(Integer key : _map.keySet()){
+//
+//}
 
     }
 }
